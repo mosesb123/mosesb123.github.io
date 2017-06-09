1d0f3cd8f122e0917c59df3ef5612a99eead5d34
mosesb123 <mosesbakst@gmail.com>
2017-06-09 00:19:49 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  console.log("PLEASE!");
-var subtexts = ["Hope you're having a nice day", "The skull bone is connected to the leg bone", "Whom?", "Witty subtext.", "O_o" ];
-var random = Math.floor(Math.random()*subtexts.length);
-$('#subtext').text(subtexts[random]);
-});
