5511881e6984e69e44a6896a671c9ef3787b9ace
mosesb123 <mosesbakst@gmail.com>
2017-06-09 00:16:07 -0400

    Create script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-$( document ).ready(function(){
-  console.log("Do I work??");
+$(document).ready(function() {
 var subtexts = ["Hope you're having a nice day", "The skull bone is connected to the leg bone", "Whom?", "Witty subtext.", "O_o" ];
-var random = Math.floor(Math.random()*subtexts.length());
+var random = Math.floor(Math.random()*subtexts.length);
 $('#subtext').text(subtexts[random]);
 });
